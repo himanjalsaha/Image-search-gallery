51568f2bebfd4e4f017e0648fbc72002a932c9d6
Himanjal saha <himanjalsaha97@gmail.com>
2024-01-07 23:00:26 +0530

    commit2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins:[ [react()],
+  legacy({
+    targets: ['defaults', 'not IE 11'],
+  })]
+  
 })
